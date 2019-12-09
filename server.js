678e1b8229d3dca97e0652447925612b4b78bedc
willf80 <willyfkouadio@gmail.com>
2019-12-09 20:17:28 +0100

    dotenv added mongoose configuration done

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track', { useUnifiedTopology: true, useNewUrlParser: true })
+// load environement configuration
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_LAB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
 
 const app = express()
 
 app.use(cors())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+  res.sendFile(`${__dirname}/views/index.html`)
+})
 
 // Not found middleware
 app.use((req, res, next) => {
-   next({status: 404, message: 'not found'})
+  next({ status: 404, message: 'not found' })
 })
 
 // Error Handling middleware
@@ -40,6 +44,6 @@ app.use((err, req, res, next) => {
     .send(errMessage)
 })
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
